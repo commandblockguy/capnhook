1f462dd88ca1d8071388eff4263b3dd3b7edc8cc
commandblockguy <commandblockguy1@gmail.com>
2020-02-29 23:00:55 -0600

    Make test user hooks more sane

--- src/test.asm
+++ src/test.asm
@@ -1,39 +1,35 @@
 public	_user_hooks
 public	_user_hook_size
 
-flags		equ	0D00080h
 flag_continue	equ	10
 
 _user_hooks:
 hook_1: ; causes all A keypresses to become B
 	db	$83
-	ld	hl,flags-flag_continue
-	ld	(hl),1
+	set	0,(iy - flag_continue)
 	cp	a,$9A
 	ret	nz
 	inc	a
-	ld	(hl),0
+	res	0,(iy - flag_continue)
 	ret
 user_hook_size = $-_user_hooks
 
 hook_2: ; causes all C keypresses to become D
 	db	$83
-	ld	hl,flags-flag_continue
-	ld	(hl),1
+	set	0,(iy - flag_continue)
 	cp	a,$9C
 	ret	nz
 	inc	a
-	ld	(hl),0
+	res	0,(iy - flag_continue)
 	ret
 
 hook_3: ; causes all E keypresses to become F
 	db	$83
-	ld	hl,flags-flag_continue
-	ld	(hl),1
+	set	0,(iy - flag_continue)
 	cp	a,$9E
 	ret	nz
 	inc	a
-	ld	(hl),0
+	res	0,(iy - flag_continue)
 	ret
 
 _user_hook_size:
